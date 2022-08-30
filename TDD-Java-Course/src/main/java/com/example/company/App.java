51636c37535d512be23bdf5d5bf198e1d71454bb
ArmandoSA485 <110858394+ArmandoSA485@users.noreply.github.com>
2022-08-30 16:07:46 -0500

    Update App.java

--- TDD-Java-Course/src/main/java/com/example/company/App.java
+++ TDD-Java-Course/src/main/java/com/example/company/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Prueba de Git desde web" );
+        System.out.println("Prueba de Armando SA");
     }
 }
